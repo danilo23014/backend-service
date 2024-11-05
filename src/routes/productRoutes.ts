802dada69c5fbae6205774858a8192f511b83f28
tu- <tu->
2024-11-05 09:57:30 -0500

    ajustes de documentacion

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -9,6 +9,16 @@ import {
 
 const productRoutes = Router();
 
+/** 
+ * @swagger
+ * /api/products:
+ * get:
+ * summary: obtener todos los productos
+ * responses:
+ * 200:
+ * description: lista de productos
+*/
+
 productRoutes.get("products/", getAllProduct);
 productRoutes.get("products/:id", getProductByID);
 productRoutes.post("products/", createProduct);
